2192c57c2bb17fdc484bd61c90ba40d720e28722
Rainer Amler <rainer@amler.at>
2023-10-23 16:10:04 +0200

    added addieren

---
+++ basic_calculations.py
@@ -0,0 +1,3 @@
+def addieren(a: int , b: int) -> int:
+    return a + b
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from basic_calculations import addieren
+
+ergebnis = addieren(3,3)
+
+print(ergebnis)
+
+print(addieren(66, 3))
