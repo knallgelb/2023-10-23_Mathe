3fb2a6590714cee087f09562a574163d0bd1fd7d
MaxMohr <mmohr@brg-pichelmayergasse.at>
2023-11-06 15:18:33 +0100

    Multiplizieren

--- test_basic_calculations.py
+++ test_basic_calculations.py
@@ -1,6 +1,5 @@
 import pytest
-
-from basic_calculations import addieren, subtrahieren
+from basic_calculations import addieren, subtrahieren, multiplizieren
 
 def test_addieren_success():
     ergebnis = addieren(3, 3)
@@ -10,3 +9,7 @@ def test_addieren_success():
 def test_subtrahieren():
     ergebnis = subtrahieren(3, 1)
     assert ergebnis == 2
+
+def test_multiplizieren_success():
+    ergebnis = multiplizieren(3, 1)
+    assert ergebnis == 3
